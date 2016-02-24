6e6950232bd1bf8988b47612ba867f9718408275
pemrouz <p3pedram@hotmail.com>
2016-02-24 01:28:44 +0000

    commit deep changes

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
-var is = require('utilise.is')
+var commit = require('utilise.set').commit
   , str = require('utilise.str')
+  , is = require('utilise.is')
 
 module.exports = function key(k, v){ 
   var set = arguments.length > 1
@@ -8,13 +9,17 @@ module.exports = function key(k, v){
 
   return function deep(o, i){
     var masked = {}
-    return !o ? undefined 
-         : !k ? o
-         : is.arr(k) ? (k.map(copy), masked)
-         : o[k] || !keys.length ? (set ? ((o[k] = is.fn(v) ? v(o[k], i) : v), o)
-                                       :   o[k])
-                                : (set ? (key(keys.join('.'), v)(o[root] ? o[root] : (o[root] = {})), o)
-                                       : key(keys.join('.'))(o[root]))
+      , ret = !o ? undefined 
+            : !k ? o
+            : is.arr(k) ? (k.map(copy), masked)
+            : o[k] || !keys.length ? (set ? ((o[k] = is.fn(v) ? v(o[k], i) : v), o)
+                                          :   o[k])
+                                   : (set ? (key(keys.join('.'), v)(o[root] ? o[root] : (o[root] = {})), o)
+                                          : key(keys.join('.'))(o[root]))
+
+    if (set) commit(o, { type: 'update', key: k, value: key(k)(o) })
+      
+    return ret
 
     function copy(k){
       var val = key(k)(o)
